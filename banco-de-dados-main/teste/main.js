cbedb8a92faef0321c65fdf5d3083c2968a95533
mauricio9002 <mauricio.golemba@escola.pr.gov.br>
2025-05-14 09:04:45 -0300

    Add files via upload

---
+++ banco-de-dados-main/teste/main.js
@@ -0,0 +1,6 @@
+document. addEventListener(DOMcontentloaded , function()){
+    const hamburger = document. getElementById(hamburger);
+    const navmenu = document.getElementById(navmenu);
+
+    hamburger.addEventListener(click , function)
+}
