5025bf9be8980259803906433b605b9d62f9eadb
mauricio9002 <mauricio.golemba@escola.pr.gov.br>
2025-05-15 11:42:50 -0300

    Delete banco-de-dados-main directory

--- banco-de-dados-main/teste/main.js
+++
@@ -1,6 +0,0 @@
-document. addEventListener(DOMcontentloaded , function()){
-    const hamburger = document. getElementById(hamburger);
-    const navmenu = document.getElementById(navmenu);
-
-    hamburger.addEventListener(click , function)
-}
